0f337cd2a83cb62d08b95289c87a2aecf9b0628a
Taylor Skilling <skilling.t@husky.neu.edu>
2017-02-24 16:15:28 -0500

    Initial commit of Motor code. Includes library and example.

---
+++ Motor/Motor.cpp
@@ -0,0 +1,52 @@
+/* 
+  Motor.cpp - Library for Ardumoto Shield
+  Created by Taylor Skilling, February 24th, 2017.
+  Based off Ardumoto Example Sketch by Jim Lindblom
+  Released into the public domain.
+
+  Three useful functions are defined:
+    setupArdumoto() -- Setup the Ardumoto Shield pins
+    driveArdumoto([motor], [direction], [speed]) -- Drive [motor] 
+      (0 for A, 1 for B) in [direction] (0 or 1) at a [speed]
+      between 0 and 255. It will spin until told to stop.
+    stopArdumoto([motor]) -- Stop driving [motor] (0 or 1).
+*/
+
+#include "Motor.h"
+
+Motor::Motor()
+{
+  // All pins should be setup as outputs:
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+
+  // Initialize all pins as low:
+  digitalWrite(PWMA, LOW);
+  digitalWrite(PWMB, LOW);
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, LOW);
+}
+
+
+// driveArdumoto drives 'motor' in 'dir' direction at 'spd' speed
+void Motor::driveArdumoto(byte motor, byte dir, byte spd)
+{
+  if (motor == MOTOR_A)
+  {
+    digitalWrite(DIRA, dir);
+    analogWrite(PWMA, spd);
+  }
+  else if (motor == MOTOR_B)
+  {
+    digitalWrite(DIRB, dir);
+    analogWrite(PWMB, spd);
+  }  
+}
+
+// stopArdumoto makes a motor stop
+void Motor::stopArdumoto(byte motor)
+{
+  driveArdumoto(motor, 0, 0);
+}
---
+++ Motor/examples/TestFunctions/TestFunctions.ino
@@ -0,0 +1,47 @@
+#include <Motor.h>
+#include <avr/sleep.h>
+
+Motor motor;
+
+void setup()
+{
+}
+
+void loop(){
+  // Drive motor A (and only motor A) at various speeds, then stop.
+  motor.driveArdumoto(MOTOR_A, CCW, 255); // Set motor A to CCW at max
+  delay(1000);  // Motor A will spin as set for 1 second
+  motor.driveArdumoto(MOTOR_A, CW, 127);  // Set motor A to CW at half
+  delay(1000);  // Motor A will keep trucking for 1 second 
+  motor.stopArdumoto(MOTOR_A);  // STOP motor A 
+
+  // Drive motor B (and only motor B) at various speeds, then stop.
+  motor.driveArdumoto(MOTOR_B, CCW, 255); // Set motor B to CCW at max
+  delay(1000);  // Motor B will spin as set for 1 second
+  motor.driveArdumoto(MOTOR_B, CW, 127);  // Set motor B to CW at half
+  delay(1000);  // Motor B will keep trucking for 1 second
+  motor.stopArdumoto(MOTOR_B);  // STOP motor B 
+
+  // Now spin both!
+  motor.driveArdumoto(MOTOR_A, CW, 255);  // Motor A at max speed.
+  motor.driveArdumoto(MOTOR_B, CW, 255);  // Motor B at max speed.
+  delay(1000);  // Drive forward for a second
+  // Now go backwards at half that speed:
+  motor.driveArdumoto(MOTOR_A, CCW, 127);  // Motor A at max speed.
+  motor.driveArdumoto(MOTOR_B, CCW, 127);  // Motor B at max speed.
+  
+  // Stop the motors
+  motor.stopArdumoto(0);
+  motor.stopArdumoto(1);
+  delay(5000);
+  
+  // Put the Arduino to sleep
+  goodnight();
+}
+
+void goodnight(){
+  cli();
+  sleep_enable();
+  sleep_cpu();
+}
+
